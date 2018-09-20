c59b1e5e2f1b8f6b70eed2b8020cc0073035770d
threadkind <threadkind@gmail.com>
2018-09-20 01:55:20 -0500

    feat: Add info window

--- src/Map.js
+++ src/Map.js
@@ -1,13 +1,26 @@
 import React from 'react'
-import { withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import { withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps"
 
 
 class Map extends React.Component {
 
+  state = {
+    infoWindowOpen : false
+  }
+
+  toggleWindow = () => {
+    if( this.state.infoWindowOpen === false ){
+      this.setState({ infoWindowOpen : true })
+    }
+    else {
+      this.setState({ infoWindowOpen : false })
+    }
+  }
 
   render() {
   	const markers = this.props.markers || []
   	return (
+
   		<GoogleMap
   			defaultZoom={10}
   			defaultCenter={{lat: 47.622451, lng: -122.352033}}>
@@ -15,7 +28,14 @@ class Map extends React.Component {
   				<Marker
             key={index}
             position={marker}
-          />
+            onClick={this.toggleWindow}
+          >
+          {this.state.infoWindowOpen && <InfoWindow
+              onCloseClick={this.toggleWindow}>
+              <div>Hello</div>
+          </InfoWindow>}
+
+          </Marker>
   			)}
   		</GoogleMap>
   	)
